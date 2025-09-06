e2a775f2f924ae4a4d3442b8ab7ca2d63157a545
TonyThuyTu <quyngaisensua@gmail.com>
2025-09-06 13:01:02 +0700

    update

--- front-end/src/app/product/page.js
+++ front-end/src/app/product/page.js
@@ -1,5 +1,10 @@
 import GetListProducts from "@/components/client/listAllProducts/getAllProducts";
 
+export const metadata = {
+  title: "All Products | MyShop",
+  description: "Browse all products in MyShop",
+};
+
 export default function ListProductsPage () {
 
     return (
