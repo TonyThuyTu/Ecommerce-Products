6c4c7c5ffee82f714b6bfb83a0adc105f5332086
TonyThuyTu <quyngaisensua@gmail.com>
2025-11-27 09:20:16 +0700

    up fe

---
+++ fe/app/products/page.tsx
@@ -0,0 +1,21 @@
+import GetListProducts from "@/components/client/listallProducts/listallProducts"
+
+export const metadata = {
+
+    title: "Products",
+
+}
+
+export default function Products () {
+
+    return (
+
+        <>
+        
+            <GetListProducts />
+
+        </>
+
+    )
+
+}
---
+++ fe/components/client/listallProducts/listallProducts.tsx
@@ -0,0 +1,14 @@
+
+export default function GetListProducts () {
+
+    return (
+
+        <>
+        
+            <h1 className="text-center mt-3 text-xl">All Products Are Here</h1>
+
+        </>
+
+    )
+
+}
