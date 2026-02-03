a48d03ad437e14920f6b5e19ae6d67de99331ef6
TonyThuyTu <quyngaisensua@gmail.com>
2026-02-03 07:27:06 +0700

    up - update remove old fe

--- fe/app/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ProductAdminPage() {
-
-  redirect('admin/products');
-
-}
--- fe/types/products.ts
+++
@@ -1,8 +0,0 @@
-export interface Product {
-    id: number;
-    name: string;
-    product_price: string;
-    img_main: string;
-    slug: string;
-    status: 0 | 1 | 2;
-}
